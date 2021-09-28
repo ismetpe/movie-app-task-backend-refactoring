7c5c1e66a08f8ee9b272239bfe8e4703815f7ad6
Ismet Perenda <ismetpe@mistral.ba>
2021-09-28 09:53:47 +0200

    seed folder added

---
+++ movie-app-task-backend/Data/Seeds/ScreeningsSeed.cs
@@ -0,0 +1,16 @@
+﻿using movie_app_task_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movie_app_task_backend.Data.Seeds
+{
+    public class ScreeningsSeed
+    {
+        public static List<Screening> Screenings { get; } = new()
+        {
+
+        };
+    }
+}
